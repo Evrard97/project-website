caa46a739d4a9f6abb89a1c95d754084edff8cfc
Evrard97 <Aka-evrard.aka@outlook.com>
2022-06-02 21:34:00 +0200

    Ecommerce mern stack debut

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <header className="p-4 bg-black">
+        <a className="text-gray-50 text-2xl font-bold" href="/">
+          Ecommerce
+        </a>
+      </header>
+      <main className="text-blue-700 text-[33px]">liste des produits</main>
+    </>
+  );
+}
+
+export default App;
